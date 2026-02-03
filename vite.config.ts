f1f65b2c1c6bf4fb501e1edbd951841b55ca1c5c
zaynrix <zaynrix@users.noreply.github.com>
2026-02-04 00:22:05 +0100

    Add deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/clearup/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
